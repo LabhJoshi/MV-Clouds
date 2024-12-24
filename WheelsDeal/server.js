62062fe5c3368da2a495becb9f3339e98a3406bc
LabhJoshi <labhjoshi25@gmail.com>
2024-12-25 00:14:17 +0530

    Completed the website

--- WheelsDeal/server.js
+++ WheelsDeal/server.js
@@ -49,7 +49,7 @@ app.get('/api/upcoming_cars', async (req, res) => {
 });
 
 app.post('/api/signup', async (req, res) => {
-    count+=1;
+    var count=101;
     const { email, username, password } = req.body;
     try {
         const result = await pool.query('INSERT INTO Users (user_id, email, username, password) VALUES ($1, $2, $3, $4)', [count, email, username, password]);
@@ -58,21 +58,22 @@ app.post('/api/signup', async (req, res) => {
         console.error(err);
         res.status(500).send('Server error');
     }
+    count++;
 });
 
 app.get('/api/login', async (req, res)=>{
     try{
         const result = await pool.query('SELECT * FROM Users');
         res.json(result.rows);
+        console.log(result.rows);
     } catch (err) {
         console.error(err);
         res.status(500).send('Server error');
-
     }
 });
 
 app.post('/api/contact', async (req, res) => {
-    count+=1;
+    var count=101;
     const { first_name, last_name, email, phone, comment } = req.body;
 
     try {
@@ -83,16 +84,41 @@ app.post('/api/contact', async (req, res) => {
         console.error(err);
         res.status(500).send('Server error');
     }
+    count++;
 });
 
 app.post('/api/sell', async (req, res) => {
-    count+=1;
-    const { name, number, email, car_name, price, fuel, transimission, year, image } = req.body;
+    var count=101;
+    const { name, number, email, car_name, price, fuel, transmission, year, image } = req.body;
 
     try {
         var query = 'INSERT INTO sell (sell_id, name, number, email, car_name, sell_price, fuel_type, transmission_type, mfg_year, image_url) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)';
-        const result = await pool.query(query, [count, name, number, email, car_name, price, fuel, transimission, year, image]);
+        const result = await pool.query(query, [count, name, number, email, car_name, price, fuel, transmission, year, image]);
         res.status(200).json(result.rows[0]);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server error');
+    }
+    count++;
+});
+
+app.get('/api/search', async (req, res) => {
+    var carname=req.query.name;
+    try {
+        const result = await pool.query('SELECT * FROM cars WHERE car_name like $1', [`%${carname}%`]);
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server error');
+    }
+});
+
+app.get('/api/car_details', async (req, res) => {
+    var car_id=req.query.car_id;
+    // console.log(car_id);
+    try{
+        const result = await pool.query('SELECT * FROM cars WHERE car_id=$1', [car_id]);
+        res.json(result.rows);
     } catch (err) {
         console.error(err);
         res.status(500).send('Server error');
